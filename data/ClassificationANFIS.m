b17aabd5e0961ff8f44320cec67a1629a08c151b
ISTiago <tiagoneves540@tecnico.ulisboa.pt>
2024-10-05 23:42:41 +0100

    Classification v2.3 - Finalized ANFIS by adding relevant performance metrics and commenting the code

--- data/ClassificationANFIS.m
+++ data/ClassificationANFIS.m
@@ -25,23 +25,55 @@ opt.NumClusters = 5;
 ts_model = genfis(X_train,Y_train,opt);
 
 %% Check initial performance on test set
+
+% Get model prediction and binarize output between 0 or 1 with threshold at 0.5
 Y_pred_initial = evalfis(ts_model, X_test);
 Y_pred_initial(Y_pred_initial>=0.5) = 1;
 Y_pred_initial(Y_pred_initial<0.5) = 0;
-class_report_initial = classperf(Y_test, Y_pred_initial);
-fprintf('Initial Accuracy: %4.3f \n', class_report_initial.CorrectRate);
-fprintf('Initial Sensitivity: %4.3f \n', class_report_initial.Sensitivity);
-fprintf('Initial Specificity: %4.3f \n', class_report_initial.Specificity);
+
+% Get model performance metrics
+initial_class_report = classperf(Y_test, Y_pred_initial);
+
+% Retrieve and calculate relevant metrics
+initial_confusion_matrix = initial_class_report.DiagnosticTable;
+initial_accuracy = initial_class_report.CorrectRate;
+initial_recall = initial_class_report.Sensitivity;
+initial_precision = initial_class_report.PositivePredictiveValue;
+initial_F1_score = 2/(1/initial_precision+1/initial_recall);
+initial_Kappa_score = 0.01^2*(sum(initial_confusion_matrix(:,1))*sum(initial_confusion_matrix(1,:))+sum(initial_confusion_matrix(:,2))*sum(initial_confusion_matrix(2,:)));
+
+% Display metrics
+fprintf('Initial Accuracy: %4.3f \n', initial_accuracy);
+fprintf('Initial Recall: %4.3f \n', initial_recall);
+fprintf('Initial Precision: %4.3f \n', initial_precision);
+fprintf('Initial F1-Score: %4.3f \n', initial_F1_score);
+fprintf('Initial Kappa Score: %4.3f \n', initial_Kappa_score);
 
 %% Tune initial model using ANFIS
 [in,out,rule] = getTunableSettings(ts_model);
 anfis_model = tunefis(ts_model,[in;out],X_train,Y_train,tunefisOptions("Method","anfis"));
 
 %% Check ANFIS tuned model performance
+
+% Get model prediction and binarize output between 0 or 1 with threshold at 0.5
 Y_pred_final = evalfis(anfis_model, X_test);
 Y_pred_final(Y_pred_final>=0.5) = 1;
 Y_pred_final(Y_pred_final<0.5) = 0;
-class_report_final = classperf(Y_test, Y_pred_final);
-fprintf('Final Accuracy: %4.3f \n', class_report_final.CorrectRate);
-fprintf('Final Sensitivity: %4.3f \n', class_report_final.Sensitivity);
-fprintf('Final Specificity: %4.3f \n', class_report_final.Specificity);
+
+% Get model performance metrics
+final_class_report = classperf(Y_test, Y_pred_final);
+
+% Retrieve and calculate relevant metrics
+final_confusion_matrix = final_class_report.DiagnosticTable;
+final_accuracy = final_class_report.CorrectRate;
+final_recall = final_class_report.Sensitivity;
+final_precision = final_class_report.PositivePredictiveValue;
+final_F1_score = 2/(1/final_precision+1/final_recall);
+final_Kappa_score = 0.01^2*(sum(final_confusion_matrix(:,1))*sum(final_confusion_matrix(1,:))+sum(final_confusion_matrix(:,2))*sum(final_confusion_matrix(2,:)));
+
+% Display metrics
+fprintf('Final Accuracy: %4.3f \n', final_accuracy);
+fprintf('Final Recall: %4.3f \n', final_recall);
+fprintf('Final Precision: %4.3f \n', final_precision);
+fprintf('Final F1-Score: %4.3f \n', final_F1_score);
+fprintf('Final Kappa Score: %4.3f \n', final_Kappa_score);
